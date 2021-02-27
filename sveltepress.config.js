4ac69f64c7075865c8dee8343497a222f870133a
brandonxiang <1542453460@qq.com>
2021-02-27 14:46:41 +0800

    chore: 公共配置修改

---
+++ sveltepress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "Brandon's Blog",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "External", link: "https://google.com" },
+    ],
+  },
+};
