6472e992c2578321e8f6d69b0bf75218fdb696d5
brandonxiang <1542453460@qq.com>
2021-08-23 20:58:18 +0800

    chore: change ali font

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -47,6 +47,7 @@
 		text-decoration: none;
 		padding: 1em 0.5em;
 		display: block;
+		font-weight:500
 	}
 </style>
 
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,10 +11,6 @@
 <svelte:head>
   <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.1/styles/github.min.css">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Overpass">
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Fira+Mono">
 </svelte:head>
 
 <style>
