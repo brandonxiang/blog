bf38fdb7b66456e1fad8ac2cf89ebbbdf115ed6e
brandonxiang <1542453460@qq.com>
2022-09-29 18:19:30 +0800

    chore: update readme

--- src/routes/keynote.svelte
+++ src/routes/keynote.svelte
@@ -1,88 +1,93 @@
 <script>
   let keynotes = [
     {
+      title: '前端测试左移',
+      url: 'https://keynote.brandonxiang.top/shift-left-testing.html',
+      date: 'Aug 03, 2022',
+    },
+    {
       title: '如何做好一个技术分享、技术方案、答辩',
-      url: 'https://keynote.vercel.app/speech.html',
+      url: 'https://keynote.brandonxiang.top/speech.html',
       date: 'Jan 07, 2021',
     },
     {
       title: '前端项目架构设计',
-      url: 'https://keynote.vercel.app/how-to-learn-fe.html',
+      url: 'https://keynote.brandonxiang.top/how-to-learn-fe.html',
       date: 'Nov 12, 2021',
     },
     {
       title: '学习前端的方法',
-      url: 'https://keynote.vercel.app/how-to-learn-fe.html',
+      url: 'https://keynote.brandonxiang.top/how-to-learn-fe.html',
       date: 'AUG 06, 2021',
     },
     {
       title: '使用Next.js实现优雅降级',
-      url: 'https://keynote.vercel.app/nextjs-fallback.html',
+      url: 'https://keynote.brandonxiang.top/nextjs-fallback.html',
       date: 'JUN 12, 2021',
     },
     {
       title: '如何参与一个开源项目',
-      url: 'https://keynote.vercel.app/github.html',
+      url: 'https://keynote.brandonxiang.top/github.html',
       date: 'APR 17, 2021',
     },
     {
       title: 'Monorepo',
-      url: 'https://keynote.vercel.app/monorepo.html',
+      url: 'https://keynote.brandonxiang.top/monorepo.html',
       date: 'MAR 05, 2021',
     },
     {
       title: 'Incremental Static Regeneration',
-      url: 'https://keynote.vercel.app/isr.html',
+      url: 'https://keynote.brandonxiang.top/isr.html',
       date: 'NOV 13, 2020',
     },
     {
       title: 'ESM Import & Bundle Free',
-      url: 'https://keynote.vercel.app/bundle-free.html',
+      url: 'https://keynote.brandonxiang.top/bundle-free.html',
       date: 'AUG 14, 2020',
     },
     {
       title: 'Svelte 入门',
-      url: 'https://keynote.vercel.app/svelte.html',
+      url: 'https://keynote.brandonxiang.top/svelte.html',
       date: 'Aug 16, 2019',
     },
     {
       title: 'React-Navigation和Expo的应用',
-      url: 'https://keynote.vercel.app/expo.html',
+      url: 'https://keynote.brandonxiang.top/expo.html',
       date: 'MAR 31, 2019',
     },
     {
       title: '微前端与Web Components',
-      url: 'https://keynote.vercel.app/microfrontend.html',
+      url: 'https://keynote.brandonxiang.top/microfrontend.html',
       date: 'APR 02, 2019',
     },
     {
       title: '小程序开发实战',
-      url: 'https://keynote.vercel.app/weapp.html',
+      url: 'https://keynote.brandonxiang.top/weapp.html',
       date: 'NOV 26, 2018',
     },
     {
       title: 'PWA开发实战',
-      url: 'https://keynote.vercel.app/pwa.html',
+      url: 'https://keynote.brandonxiang.top/pwa.html',
       date: 'NOV 26, 2018',
     },
     {
        title: 'Vue-cli3.0',
-       url: 'https://keynote.vercel.app/vue-cli.html',
+       url: 'https://keynote.brandonxiang.top/vue-cli.html',
        date: 'OCT 18, 2018',
     },
     {
        title: 'Vue单元测试',
-       url: 'https://keynote.vercel.app/vue-test.html',
+       url: 'https://keynote.brandonxiang.top/vue-test.html',
        date: 'NOV 08, 2018',
     },
     {
       title: 'Vue开发规范',
-      url: 'https://keynote.vercel.app/vue-specification.html',
+      url: 'https://keynote.brandonxiang.top/vue-specification.html',
       date: 'OCT 12, 2017'
     },
     {
       title: 'Vue技术选型与Webpack入门',
-      url: 'https://keynote.vercel.app/vue-startup.html',
+      url: 'https://keynote.brandonxiang.top/vue-startup.html',
       date: 'JUN 09, 2017',
     },
   ]
