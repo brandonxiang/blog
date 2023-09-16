3b3c4301b7b81502fd0a1e3ae7c22e80aa14dcd5
brandonxiang <1542453460@qq.com>
2023-09-16 11:01:38 +0800

    chore: update apps

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	let apps = [
 		{
+			title: 'brandon notion',
+			url: "https://brandonxiang.notion.site/brandonxiang-notion-078346416d174cb0bf79e0023ef7d8af?pvs=4",
+			description: '不知名前端notion笔记',
+		},
+		{
 			title: 'pwaland',
 			url: 'https://pwaland.brandonxiang.top/',
 			description: 'PWA应用推荐网站'
@@ -10,16 +15,57 @@
 			url: 'https://pb.brandonxiang.top/',
 			description: 'Protobuffer转typescript类型定义文件'
 		},
+
+		{
+			title: 'svelteup',
+			url: 'https://github.com/brandonxiang/svelteup',
+			description: 'web component + svelte + esbuild = svelteup'
+		},
 		{
 			title: 'FolderBsdp',
 			url: 'https://github.com/peichen3141/FolderBsdp',
 			description: '一种改良过后的bsdp差分算法'
 		},
 		{
+			title: 'babel-plugin-react-native-style-adaptation',
+			url: 'https://github.com/kukudeshiyi/babel-plugin-react-native-style-adaptation',
+			description: 'react native 样式适配神器'
+		}, 
+		{
+			title: 'ts-unused-exports',
+			url: 'https://github.com/pzavolinsky/ts-unused-exports',
+			description: '文件、代码 exports 剪枝神器'
+		},
+		{
+			title: 'i18n-shaking',
+			url: 'https://github.com/kukudeshiyi/i18n-shaking',
+			description: '基于语法树的 i18n 剪枝神器'
+		},
+		{
+			title: 'vite-antd-starter',
+			url: 'https://github.com/brandonxiang/vite-antd-starter',
+			description: '极简的 vite + antd 初始化模版'
+		},
+		{
+			title: 'vite-antd-mobile-starter',
+			url: 'https://github.com/brandonxiang/vite-antd-mobile-starter',
+			description: '极简的 vite + antd-mobile 初始化模版'
+		},
+		{
+			title: 'nextjs-antd-starter',
+			url: 'https://github.com/brandonxiang/nextjs-antd-starter',
+			description: '极简的 nextjs + antd-mobile 初始化模版'
+		},
+		{
+			title: 'geojson-python-utils',
+			url: 'https://github.com/brandonxiang/geojson-python-utils',
+			description: 'geojson 通用方法 python 实现'
+		},
+		{
 			title: 'psm',
 			url: 'https://github.com/brandonxiang/psm',
-			description: 'python换源工具'
-		}
+			description: 'python 换源工具'
+		},
 	];
 </script>
 
