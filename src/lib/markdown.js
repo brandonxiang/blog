8b750fa8db8123ab88ee3724c21d9867e95c7227
brandonxiang <1542453460@qq.com>
2023-12-19 01:11:23 +0800

    chore: update dependencies

--- src/lib/markdown.js
+++ src/lib/markdown.js
@@ -1,5 +1,5 @@
-import vfile from 'to-vfile';
-import unified from 'unified';
+import {readSync} from 'to-vfile';
+import {unified} from 'unified';
 import parse from 'remark-parse';
 import gfm from 'remark-gfm';
 import remark2rehype from 'remark-rehype';
@@ -13,12 +13,19 @@ const parser = unified().use(parse).use(gfm).use(frontmatter, ['yaml']);
 
 const runner = unified().use(remark2rehype).use(highlight).use(rehypeStringify);
 
+/**
+ * 
+ * @param {string} filename 
+ * @returns 
+ */
 export function process(filename) {
 	const slug = filename.split('.')[0];
-	const tree = parser.parse(vfile.readSync(filename));
-	let metadata = null;
+	const tree = parser.parse(readSync(filename));
+	/** @type {{date: string}} */
+	let metadata = {date: ''};
 
 	if (tree.children.length > 0 && tree.children[0].type == 'yaml') {
+		//@ts-ignore
 		metadata = yaml.load(tree.children[0].value);
 		tree.children = tree.children.slice(1, tree.children.length);
 		metadata.date = dayjs(metadata.date).format('MMM D, YYYY');
